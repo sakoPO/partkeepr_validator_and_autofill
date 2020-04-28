a0c35258f83e8d548124dca7f27ab72e0c6bafea
sakoPO <you@example.com>
2020-04-28 05:05:46 +0200

    Added resistors test.

---
+++ common.py
@@ -0,0 +1,105 @@
+from decimal import Decimal
+import sys
+
+sys.path.insert(1, "third_party/partname-resolver")
+from partname_resolver.resistors.resistor import Resistor
+from partname_resolver.units.resistanceTolerance import Tolerance as ResistanceTolerance
+from partname_resolver.units.temperature import TemperatureRange
+from partname_resolver.units.capacitanceTolerance import Tolerance as CapacitanceTolerance
+from partname_resolver.capacitors.capacitor import Capacitor
+from partname_resolver.units.capacitance import Capacitance, CapacitanceRange
+
+
+def tolerance_from_partkeepr_json(tolerance_json):
+    """Convert partkeepr json tolerance parameter to Tolerance object"""
+    if 'unit' in tolerance_json and tolerance_json["unit"] == 'Ohm':
+        return ResistanceTolerance(str(tolerance_json["value"]) + "R")
+    else:
+        if tolerance_json['value'] is not None:
+            return ResistanceTolerance(str(tolerance_json["value"]) + "%")
+        else:
+            return ResistanceTolerance(str(tolerance_json["valueMin"]) + "%", "+" + str(tolerance_json["valueMax"]) + "%")
+
+
+def working_temperature_range_from_partkeepr_json(parameters):
+    if 'Working Temperature' in parameters:
+        working_temperature = TemperatureRange(Decimal(parameters['Working Temperature']['valueMin']),
+                                               Decimal(parameters['Working Temperature']['valueMax']))
+        return working_temperature
+
+
+def resistor_from_partkeepr_json(resistor_json):
+    parameters = resistor_json["parameters"]
+    if len(resistor_json["manufacturers"]) > 0:
+        part_number = resistor_json["manufacturers"][0]['partNumber']
+        manufacturer_name = resistor_json["manufacturers"][0]['name']
+    else:
+        part_number = resistor_json['name']
+        manufacturer_name = None
+    if 'Part Type' in parameters:
+        resistor_type = Resistor.Type(parameters["Part Type"]['value'])
+    else:
+        resistor_type = None  # Resistor.Type.ThinFilmResistor  # Resistor.Type(parameters["Resistor Type"]['value'])
+    working_temperature = working_temperature_range_from_partkeepr_json(parameters)
+    resistance = parameters["Resistance"]["value"]
+    tolerance = tolerance_from_partkeepr_json(parameters['Tolerance']) if 'Tolerance' in parameters else None
+    return Resistor(resistor_type=resistor_type,
+                    manufacturer=manufacturer_name,
+                    partnumber=part_number,
+                    working_temperature_range=working_temperature,
+                    series=None,
+                    resistance=resistance,
+                    tolerance=tolerance,
+                    power=parameters["Power"]['valueMax'] if 'Power' in parameters else None,
+                    max_working_voltage=parameters["Voltage"]['valueMax'] + "V" if 'Voltage' in parameters else None,
+                    case=resistor_json['footprint'],
+                    note="")
+
+
+def capacitanceTolerance_from_partkeepr_json(tolerance_json):
+    """Convert partkeepr json tolerance parameter to Tolerance object"""
+    if 'unit' in tolerance_json and tolerance_json["unit"] == 'Farad':
+        return CapacitanceTolerance(str(tolerance_json["value"]) + "F")
+    else:
+        if tolerance_json['value'] is not None:
+            return CapacitanceTolerance(str(tolerance_json["value"]) + "%")
+        else:
+            return CapacitanceTolerance(str(tolerance_json["valueMin"]) + "%", "+" + str(tolerance_json["valueMax"]) + "%")
+
+
+def capacitor_from_partkeepr_json(capacitor_json):
+    try:
+        parameters = capacitor_json["parameters"]
+        if len(capacitor_json["manufacturers"]) > 0:
+            part_number = capacitor_json["manufacturers"][0]['partNumber']
+            manufacturer_name = capacitor_json["manufacturers"][0]['name']
+        else:
+            part_number = capacitor_json['name']
+            manufacturer_name = None
+        capacitor_type = Capacitor.Type(parameters["Capacitor Type"]['value'])
+        if capacitor_type == Capacitor.Type.CeramicTrimmer:
+            capacitance = CapacitanceRange(parameters["Capacitance"]["valueMin"], parameters["Capacitance"]["valueMax"])
+        else:
+            capacitance = parameters["Capacitance"]["value"]
+        working_temperature = working_temperature_range_from_partkeepr_json(parameters)
+        if 'Voltage' in parameters:
+            if parameters['Voltage']['valueMax'] is not None:
+                voltage = parameters['Voltage']['valueMax'] + "V"
+            else:
+                voltage = parameters["Voltage"]['value'] + 'V'
+        else:
+            voltage = None
+        return Capacitor(capacitor_type=capacitor_type,
+                         manufacturer=manufacturer_name,
+                         partnumber=part_number,
+                         working_temperature_range=working_temperature,
+                         series=None,
+                         capacitance=capacitance,
+                         voltage=voltage,
+                         tolerance=capacitanceTolerance_from_partkeepr_json(parameters['Tolerance']) if 'Tolerance' in parameters else None,
+                         dielectric_type=parameters['Dielectric Type']['value'] if 'Dielectric Type' in parameters else None,
+                         case=capacitor_json['footprint'] if len(capacitor_json['footprint']) > 0 else None,
+                         note="")
+    except TypeError:
+        print(capacitor_json)
+        raise
---
+++ resistors_test.py
@@ -0,0 +1,117 @@
+from common import *
+from unittest import TestCase
+import json
+import skip_resistors_test
+import sys
+
+sys.path.insert(1, "third_party/partname-resolver")
+from partname_resolver.resistors import resistors_partname_decoder
+from partname_resolver.units.resistance import Resistance
+from partname_resolver.units.power import Power
+
+
+class TestResistors(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        with open("resistors.json") as file:
+            cls.resistors = json.load(file)
+
+    def test_single_manufacturer_per_part(self):
+        for resistor in self.resistors:
+            with self.subTest(resistor['name']):
+                if resistor['partkeepr_id'] in skip_resistors_test.test_single_manufacturer_per_part:
+                    self.skipTest("part in skip list")
+                self.assertEqual(len(resistor["manufacturers"]), 1)
+
+    def test_part_name_equal_manufacturer_part_name(self):
+        for resistor in self.resistors:
+            with self.subTest(resistor['name']):
+                if len(resistor["manufacturers"]) > 0:
+                    self.assertEqual(resistor["name"], resistor["manufacturers"][0]["partNumber"])
+
+    def test_has_production_remarks(self):
+        for resistor in self.resistors:
+            with self.subTest(resistor['name']):
+                self.assertIn(resistor["productionRemarks"], ["SMD", "SMT", "THT", "Screw"])
+
+    def test_has_resistance_parameter(self):
+        for resistor in self.resistors:
+            with self.subTest(resistor['name']):
+                if resistor['partkeepr_id'] in skip_resistors_test.test_has_resistance_parameter:
+                    self.skipTest(str(resistor['name']) + " part in skip list")
+                self.assertIn("Resistance", resistor["parameters"])
+                resistance = resistor["parameters"]["Resistance"]
+                self.assertIsNotNone(resistance["value"])
+                self.assertEqual(resistance["unit"], "Ohm")
+
+    def test_has_tolerance_parameter(self):
+        for resistor in self.resistors:
+            with self.subTest(resistor['name']):
+                if resistor['partkeepr_id'] in skip_resistors_test.test_has_tolerance_parameter:
+                    self.skipTest(str(resistor['name']) + " part in skip list")
+                self.assertIn("Tolerance", resistor["parameters"])
+                tolerance = resistor["parameters"]["Tolerance"]
+                self.assertIsNotNone(tolerance["value"])
+
+    def test_has_voltage_parameter(self):
+        for resistor in self.resistors:
+            with self.subTest(resistor['name'] + " (" + resistor['partkeepr_id'].replace("/api/parts/", "") + ")"):
+                if resistor['partkeepr_id'] in skip_resistors_test.test_has_voltage_parameter:
+                    self.skipTest(str(resistor['name']) + " part in skip list")
+                self.assertIn("Voltage", resistor["parameters"])
+                self.assertIsNotNone(resistor["parameters"]['Voltage']['valueMax'])
+
+    def test_has_power_parameter(self):
+        for resistor in self.resistors:
+            with self.subTest(resistor['name']):
+                if resistor['partkeepr_id'] in skip_resistors_test.test_has_power_parameter:
+                    self.skipTest(str(resistor['name']) + " part in skip list")
+                self.assertIn("Power", resistor["parameters"])
+                power = resistor["parameters"]["Power"]
+                self.assertEqual(power["unit"], "Watt")
+                self.assertIsNone(power["value"])
+                self.assertIsNone(power["valueMin"])
+                self.assertIsNotNone(power["valueMax"])
+
+    def test_footprint(self):
+        for resistor in self.resistors:
+            with self.subTest(resistor['name']):
+                if resistor["productionRemarks"] == "THT":
+                    self.assertTrue(True)
+                elif resistor["productionRemarks"] == "SMD":
+                    self.assertIn(resistor["footprint"], ["0402", "0603", "0805", "1206", "2512"])
+
+    def test_description_format(self):
+        for resistor in self.resistors:
+            with self.subTest(resistor['name']):
+                res = None
+                if len(resistor['manufacturers']):
+                    manufacturer_part_number = resistor["manufacturers"][0]['partNumber']
+                    res = resistors_partname_decoder.resolve(manufacturer_part_number)
+                if res is None:
+                    res = resistor_from_partkeepr_json(resistor)
+                self.assertIsNotNone(res)
+                self.assertEqual(res.get_description(), resistor["description"])
+
+    def test_parameters_equal_decoded_parameters_from_partname(self):
+        for resistor in self.resistors:
+            if len(resistor['manufacturers']):
+                manufacturer_part_number = resistor["manufacturers"][0]['partNumber']
+                if len(manufacturer_part_number) == 0:
+                    continue
+                with self.subTest(resistor['name']):
+                    if resistor[
+                        'partkeepr_id'] in skip_resistors_test.test_parameters_equal_decoded_parameters_from_partname:
+                        self.skipTest(str(resistor['name']) + " part in skip list")
+                    parameters = resistor["parameters"]
+                    tolerance = tolerance_from_partkeepr_json(parameters["Tolerance"])
+                    working_temperature = working_temperature_range_from_partkeepr_json(parameters)
+                    decoded_parameters = resistors_partname_decoder.resolve(manufacturer_part_number)
+                    self.assertIsNotNone(decoded_parameters)
+                    self.assertEqual(Resistance(parameters["Resistance"]["value"]), decoded_parameters.resistance)
+                    self.assertEqual(Power(parameters["Power"]["valueMax"]), decoded_parameters.power)
+                    self.assertEqual(tolerance, decoded_parameters.tolerance)
+                    self.assertEqual(decoded_parameters.max_working_voltage,
+                                     str(parameters['Voltage']['valueMax']) + "V")
+                    if working_temperature is not None:
+                        self.assertEqual(decoded_parameters.working_temperature_range, working_temperature)
